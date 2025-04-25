9e751a48b272efdb6241a41b163ee1375d95de88
sharp541 <stonegate541@gmail.com>
2025-04-25 13:46:22 +0900

    Add Japanese dataset

--- python/dataset.py
+++ python/dataset.py
@@ -1,9 +1,11 @@
 # %%
 from datasets import load_dataset
 from huggingface_hub import login
+import pykakasi
 import re
 import json
 import os
+
 
 # %%
 if not os.path.exists("./.env/token.json"):
@@ -14,47 +16,68 @@ else:
         access_key = config["access_key"]
         os.environ["CURL_CA_BUNDLE"] = config["ca_bundle"]
         login(access_key)
+# %%
 dataset = load_dataset("bigcode/starcoderdata",
                        data_dir="python", split="train", streaming=True)
-
+# %%
+ja_dataset= load_dataset("izumi-lab/cc100-ja-filter-ja-normal", split="train", streaming=True, ignore_verifications=True)
 
 # %%
 qwerty_layout = set([
     'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k',
     'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ' '
 ])
-# %%
-sampled_files = []
-total_size = 0
-max_size = 1 * 1024 * 1024  # 1MB
 
-for entry in dataset:
-    content = entry["content"]
-    size = len(content.encode("utf-8"))
-    if total_size + size > max_size:
-        break
-    sampled_files.append(content)
-    total_size += size
-
-# %%
 def clean(text):
     t = re.sub(r"\*+", "*", "".join(c if c in qwerty_layout else "" for c in text.lower())).strip()
     # remove multiple spaces
     t = re.sub(r"\s+", " ", t)
     return t
 
+def sample_dataset(dataset, selector , max_size=1 * 1024 * 1024):
+    sampled_files = []
+    total_size = 0
+    for entry in dataset:
+        content = selector(entry)
+        if not content:
+            raise ValueError("selector returned empty content")
+        size = len(content.encode("utf-8"))
+        if total_size + size > max_size:
+            break
+        sampled_files.append(content)
+        total_size += size
+    return "".join(sampled_files)
+# %%
 en = "../data/en.txt"
 with open(en, "w", encoding="utf-8") as f:
+    selector = lambda x: x["content"] if "content" in x else None
+    sampled_files = sample_dataset(dataset, max_size=1 * 1024 * 1024)
     text = "".join(sampled_files)
     cleaned = clean(text)
     f.write(f"{cleaned}\n")
 
 # %%
+ja_raw = "../data/ja_raw.txt"
 ja = "../data/ja.txt"
-with open(ja, "r", encoding="utf-8") as f:
-    text = f.read()
+# %%
+with open(ja_raw, "w", encoding="utf-8") as f:
+    selector = lambda x: x["text"] if "text" in x else None
+    sampled_files = sample_dataset(ja_dataset, selector, max_size=2 * 1024 * 1024)
+    text = "\n".join(sampled_files)
+    # cleaned = clean(text)
+    f.write(f"{text}\n")
 
+# %%
+with open(ja_raw, "r", encoding="utf-8") as f:
+    text = f.read().strip()
+
+kks = pykakasi.kakasi()
 with open(ja, "w", encoding="utf-8") as f:
-    f.write(clean(text))
+    for line in text.split("\n"):
+        if not line.strip():
+            continue
+        result = kks.convert(line)
+        f.write("".join([item["hepburn"] for item in result]))
+        f.write("\n")
 
 # %%

dc078540bc491ab0b43b98c1bdebfa6c9e2f53a6
sharp541 <stonegate541@gmail.com>
2025-11-26 20:58:36 +0900

    refactor: clean up main function by removing unused layouts and optimizing layout initialization

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::path::Path;
 use std::collections::HashSet;
+use std::path::Path;
 
 use keyboard_layout_optimizer::algorithms::Genetic;
+use keyboard_layout_optimizer::keyboard_layout::Finger as F;
 use keyboard_layout_optimizer::keyboard_layout::*;
 use keyboard_layout_optimizer::n_gram::NGramDB;
-use keyboard_layout_optimizer::keyboard_layout::Finger as F;
 
 fn main() -> Result<(), std::io::Error> {
     let source_paths = vec![Path::new("data/ja.txt"), Path::new("data/en.txt")];
@@ -29,34 +29,13 @@ fn main() -> Result<(), std::io::Error> {
         [F::P, F::R, F::M, F::I, F::I, F::I, F::I, F::M, F::R, F::P],
         [F::P, F::R, F::M, F::I, F::I, F::I, F::I, F::M, F::R, F::P],
     ];
-    let mut physical_layout = PhysicalLayout::new(cost_table, finger_table.clone()).expect("Invalid cost table");
+    let mut physical_layout =
+        PhysicalLayout::new(cost_table, finger_table).expect("Invalid cost table");
     physical_layout.calculate_tri_gram_cost();
     let mut normal_physical_layout =
         PhysicalLayout::new(normal_cost_table, finger_table).expect("Invalid cost table");
     normal_physical_layout.calculate_tri_gram_cost();
 
-    let usable_chars = vec![
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '*', ';', '/', '+',
-    ];
-
-    let qwerty_layout = vec![
-        'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k',
-        'l', ';', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
-    ];
-    let ohnishi_layout = vec![
-        'q', 'l', 'u', ';', '.', 'f', 'w', 'r', 'y', 'p', 'e', 'i', 'a', 'o', ',', 'k', 't', 'n',
-        's', 'h', 'z', 'x', 'c', 'v', '/', 'g', 'd', 'm', 'j', 'b',
-    ];
-    let astarte_layout = vec![
-        'q', 'p', 'u', 'y', ',', 'j', 'd', 'h', 'g', 'w', 'i', 'o', 'e', 'a', '.', 'k', 't', 'n',
-        's', 'r', 'z', 'x', '/', 'c', ';', 'm', 'l', 'f', 'b', 'v',
-    ];
-
-    let eucalyn_layout = vec![
-        'q', 'w', ',', '.', ';', 'm', 'r', 'd', 'y', 'p', 'a', 'o', 'e', 'i', 'u', 'g', 't', 'k',
-        's', 'n', 'z', 'x', 'c', 'v', 'f', 'b', 'h', 'j', 'l', '/',
-    ];
     let custom_layout = vec![
         'h', 'k', 'r', 'z', 'q', '+', ';', 'e', 'p', 'v', // upper row
         'm', 's', 'n', 't', 'g', 'c', 'a', 'o', 'i', 'u', // middle row
@@ -67,22 +46,6 @@ fn main() -> Result<(), std::io::Error> {
     let tri_grams = n_gram_db
         .get_tri_grams(&custom_layout_set)
         .expect("Failed to get tri grams");
-
-    let qwerty = LogicalLayout::from_usable_chars(&normal_physical_layout, qwerty_layout.clone());
-    let score = qwerty.evaluate(&normal_physical_layout, &tri_grams);
-    println!("qwerty score: {}", score);
-
-    let ohnishi = LogicalLayout::from_usable_chars(&normal_physical_layout, ohnishi_layout.clone());
-    let score = ohnishi.evaluate(&normal_physical_layout, &tri_grams);
-    println!("ohnishi score: {}", score);
-
-    let astarte = LogicalLayout::from_usable_chars(&normal_physical_layout, astarte_layout.clone());
-    let score = astarte.evaluate(&normal_physical_layout, &tri_grams);
-    println!("astarte score: {}", score);
-
-    let eucalyn = LogicalLayout::from_usable_chars(&normal_physical_layout, eucalyn_layout.clone());
-    let score = eucalyn.evaluate(&normal_physical_layout, &tri_grams);
-    println!("eucalyn score: {}", score);
 
     let custom = LogicalLayout::from_usable_chars(&physical_layout, custom_layout.clone());
     let score = custom.evaluate(&physical_layout, &tri_grams);
